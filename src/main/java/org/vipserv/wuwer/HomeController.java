2f286b12967a650fd7c0f6e05a846b4f4cc363d2
Tomek <wawrotomasz@gmail.com>
2013-05-03 22:44:08 +0200

    Tutorial stuff: user input

--- src/main/java/org/vipserv/wuwer/HomeController.java
+++ src/main/java/org/vipserv/wuwer/HomeController.java
@@ -1,15 +1,18 @@
 package org.vipserv.wuwer;
 
 import java.text.DateFormat;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Handles requests for the application home page.
@@ -17,7 +20,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	
+	@Autowired
+	private Comparator<String> comparator;
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -35,6 +42,20 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	
+	@RequestMapping(value = "/compare", method = RequestMethod.GET)
+	public String compare(@RequestParam("input1") String input1,
+			@RequestParam("input2") String input2, Model model)
+	{
+		int result = comparator.compare(input1, input2);
+		
+		String inEnglish = (result < 0) ? "less than" : (result > 0 ? "greater than" : "equal to");
+        String output = "According to our Comparator, '" + input1 + "' is " + inEnglish + "'" + input2 + "'";
+ 
+        model.addAttribute("output", output);
+
+		return "compareResult";
 	}
 	
 }
