ffc3e7669fa0de2081006f7587b46575894adc0a
Tomek <wawrotomasz@gmail.com>
2013-06-17 12:04:27 +0200

    Add stopargs to Service model class

--- src/main/java/pl/edu/agh/iosr/virtgraph/model/Service.java
+++ src/main/java/pl/edu/agh/iosr/virtgraph/model/Service.java
@@ -10,24 +10,44 @@ public class Service {
 	private String runCommand;
 	private String stopCommand;
 	private boolean start;
-	private List<String> args;
+	private List<String> startArgs;
+	private List<String> stopArgs;
+
+	public List<String> getStartArgs() {
+		return startArgs;
+	}
+
+	public void setStartArgs(List<String> startArgs) {
+		this.startArgs = startArgs;
+	}
+
+	public List<String> getStopArgs() {
+		return stopArgs;
+	}
+
+	public void setStopArgs(List<String> stopArgs) {
+		this.stopArgs = stopArgs;
+	}
+
+	public List<String> getstartArgs() {
+		return startArgs;
+	}
+
+	Long id;
 
 	public Service(String name, String runCommand, String stopCommand,
-			boolean start, List<String> args) {
+			boolean start, List<String> startArgs, List<String> stopArgs) {
 		super();
 		this.name = name;
 		this.runCommand = runCommand;
 		this.stopCommand = stopCommand;
 		this.start = start;
-		this.args = args;
+		this.startArgs = startArgs;
+		this.stopArgs = stopArgs;
 	}
 
 	public List<String> getArgs() {
-		return args;
-	}
-
-	public void setArgs(List<String> args) {
-		this.args = args;
+		return startArgs;
 	}
 
 	public Service() {
@@ -63,6 +83,94 @@ public class Service {
 
 	public void setStopCommand(String stopCommand) {
 		this.stopCommand = stopCommand;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((startArgs == null) ? 0 : startArgs.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((runCommand == null) ? 0 : runCommand.hashCode());
+		result = prime * result
+				+ ((serviceList == null) ? 0 : serviceList.hashCode());
+		result = prime * result + (start ? 1231 : 1237);
+		result = prime * result
+				+ ((stopCommand == null) ? 0 : stopCommand.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Service other = (Service) obj;
+		if (startArgs == null) {
+			if (other.startArgs != null) {
+				return false;
+			}
+		} else if (!startArgs.equals(other.startArgs)) {
+			return false;
+		}
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		if (runCommand == null) {
+			if (other.runCommand != null) {
+				return false;
+			}
+		} else if (!runCommand.equals(other.runCommand)) {
+			return false;
+		}
+		if (serviceList == null) {
+			if (other.serviceList != null) {
+				return false;
+			}
+		} else if (!serviceList.equals(other.serviceList)) {
+			return false;
+		}
+		if (start != other.start) {
+			return false;
+		}
+		if (stopCommand == null) {
+			if (other.stopCommand != null) {
+				return false;
+			}
+		} else if (!stopCommand.equals(other.stopCommand)) {
+			return false;
+		}
+		return true;
+	}
+
+	private ServiceList serviceList;
+
+	public void setServiceList(ServiceList serviceList) {
+		this.serviceList = serviceList;
+	}
+
+	public ServiceList getServiceList() {
+		return serviceList;
 	}
 
 }
